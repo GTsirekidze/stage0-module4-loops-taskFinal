980b2bf1163ac2cb3a43866903a24fcdc7ef9584
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-09-21 16:47:01 +0400

    "commit"

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int completeLength = cathetusLength*2-1;
+        for(int i=0;i<cathetusLength;i++){
+            for (int j=0;j<completeLength;j++){
+                if(j<=completeLength/2+i && j>=completeLength/2-i) System.out.print(Math.abs(j-completeLength/2)+1);
+                else if(j<completeLength/2) System.out.print(" ");
+            }
 
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
